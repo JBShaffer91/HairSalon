c8602bea55abc1bd7284445d1311dda5cdae0ced
Justin Shaffer <justinbshaffer91@gmail.com>
2023-05-26 15:37:12 -0700

    Add search views for Stylists and Clients, and create Appointment model
    - Created Views/Stylists/Search.cshtml for searching stylists by name.
    - Created Views/Clients/Search.cshtml for searching clients by name.
    - Created Models/Appointment.cs for appointment tracking feature.

---
+++ HairSalon/Models/Appointment.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace HairSalon.Models
+{
+  public class Appointment
+  {
+    public int AppointmentId { get; set; }
+    public int StylistId { get; set; }
+    public int ClientId { get; set; }
+    public DateTime AppointmentDateTime { get; set; }
+
+    public virtual Stylist Stylist { get; set; }
+    public virtual Client Client { get; set; }
+  }
+}
