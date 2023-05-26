94fa6be1452cdf64ad82e2808860b376512d0492
Justin Shaffer <justinbshaffer91@gmail.com>
2023-05-26 09:54:58 -0700

    Add Stylists and Clients controllers
    This commit includes the following updates:
    
    - Add the StylistsController which handles CRUD operations for the Stylist model.
      - It includes methods for Index, Create (both GET and POST), and Details actions.
      - The Details action includes logic to load related Clients using Entity Framework Core's Include method.
    
    - Add the ClientsController which handles CRUD operations for the Client model.
      - It includes methods for Index, Create (both GET and POST), and Details actions.
      - The Details action includes logic to load related Stylist using Entity Framework Core's Include method.
      - The Create action includes logic to populate a SelectList of Stylists for a drop-down in the view.

---
+++ HairSalon/Controllers/ClientsController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HairSalon.Models;
+using System.Linq;
+
+namespace HairSalon.Controllers
+{
+  public class ClientsController : Controller
+  {
+    private HairSalonContext _db;
+
+    public ClientsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Clients.Include(client => client.Stylist).ToList());
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Client thisClient = _db.Clients
+      .Include(client => client.Stylist)
+      .FirstOrDefault(client => client.ClientId == id);
+      return View(thisClient);
+    }
+  }
+}
